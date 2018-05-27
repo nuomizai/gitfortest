28d53fa0ebf61c3d7fc95813a2a8fafa63ffb5f4
nuomizai <769572294@qq.com>
2018-05-27 11:56:35 +0800

    lalala
    asdf

---
+++ mymymy/mymymy/mymymy.cpp
@@ -0,0 +1,14 @@
+// mymymy.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Merry Christmas ,hohoo" << endl;
+	return 0;
+}
+
